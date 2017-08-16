5583e39d87012ba02d584dcc444c6bd1ff0225cb
chung-edison <eddy2809@hotmail.com>
2017-08-15 21:29:20 -0500

    Layout + index

---
+++ WEBAPP/WEBAPP/Controllers/PartialController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBAPP;
+
+namespace ProyectoWeb.Controllers
+{
+    public class PartialController : Controller
+    {
+        [HttpGet]
+        public ActionResult CategoriaList()
+        {
+            ModelStoreContainer CL = new ModelStoreContainer();
+            // Your user information in HeaderModel
+            return PartialView("CategoriaList", CL.CategoriaSet.ToList());
+        }
+    }
+}
